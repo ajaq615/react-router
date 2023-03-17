fa70eb3fdc134941d17fe0cb08da1dc666ef5a81
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-17 14:32:59 +0800

    Changes to be committed: 	new file:   src/components/MainNavigation.js 	new file:   src/pages/Home.js
    Changes not staged for commit:
    	modified:   src/App.js
    	modified:   src/index.css
    
    Untracked files:
    	src/components/MainNavigation.module.css
    	src/pages/Error.js
    	src/pages/ProductDetails.js
    	src/pages/Products.js
    	src/pages/Root.js

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : '')}
+              to='/'
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : '')}
+              to='/products'
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
