27023d77504e2ace77e11ce1e17b01a948b62694
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-17 11:04:17 +0800

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  //createRoutesFromElements,
+  //Route
+} from 'react-router-dom';
+import HomePage from './pages/Home';
+import Products from './pages/Products';
 
+//FOR JSX/COMPONENT-based way of defining routes (old version)
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<Products />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinitions);
+
+//OBJECT-based way of defining routes
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <Products /> },
+]);
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
